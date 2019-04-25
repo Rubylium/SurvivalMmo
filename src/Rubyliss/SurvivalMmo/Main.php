a21a1a5a276e302edb49a2edf73abb5266c0ecc5
Rubyliss <alexisoko0@gmail.com>
2019-04-25 12:33:39 +0200

    Add files via upload

---
+++ src/Rubyliss/SurvivalMmo/Main.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rubyliss\SurvivalMmo;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+
+class Main extends PluginBase{
+
+	public function onEnable() : void{
+		$this->getLogger()->info("Hello World!");
+	}
+
+	public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
+		switch($command->getName()){
+			case "examplecommand":
+				$sender->sendMessage("Example command output");
+				return true;
+			default:
+				return false;
+		}
+	}
+
+	public function onDisable() : void{
+		$this->getLogger()->info("Bye");
+	}
+}
